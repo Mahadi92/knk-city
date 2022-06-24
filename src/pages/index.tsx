b67a68549af1fc87f48320d61ceff57b9ae9b397
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2022-06-25 03:04:23 +0600

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import DefaultLayout from "@/components/layouts/DefaultLayout";
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <DefaultLayout>
+      <div>hello</div>
+    </DefaultLayout>
+  );
+};
+
+export default Home;
